bb1a8f3605d4986cfdd51159f5fbd4fb8b669821
october-03 <season_autumn@kakao.com>
2023-07-19 17:02:02 +0900

    데이터타입 정리

--- chapter01/MyPlayground.playground/Contents.swift
+++ chapter01/MyPlayground.playground/Contents.swift
@@ -6,3 +6,32 @@ var b = 100;
 b = 200;
 
 b;
+
+// Int : 64bit 정수형
+var int: Int = -100;
+int;
+
+// UInt : 부호가 없는 64bit 정수형
+var uInt: UInt = 100;
+
+// Float : 32bit 부동 소수점
+var float: Float = 1.1;
+
+// Double : 64bit 부동 소수점
+var double: Double = 1.1;
+
+// Bool : boolean 값
+var bool: Bool = true;
+
+// Character : 문자
+var character: Character = "가"
+
+// String : 문자열
+var string: String = "가나다"
+
+// Any : 모든 타입을 지칭
+var any: Any = 1.1;
+any = -1.1;
+any = 100;
+any = -100;
+any = false;
