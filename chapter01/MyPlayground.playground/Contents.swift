2935ac1981e1a5e63b7eea51f342af341561c1f2
october-03 <season_autumn@kakao.com>
2023-07-19 17:27:33 +0900

    콜렉션타입 정리

--- chapter01/MyPlayground.playground/Contents.swift
+++ chapter01/MyPlayground.playground/Contents.swift
@@ -35,3 +35,28 @@ any = -1.1;
 any = 100;
 any = -100;
 any = false;
+
+// Array : 특정 데이터 타입의 값들을 순서대로 저장하는 리스트 (Any 가능)
+var numbers: [Int] = [];
+numbers.append(int);
+numbers.insert(140, at: 0);
+numbers.remove(at: 1);
+
+// Dictionary : 순서없이 키와 값이 한 쌍으로 데이터를 저장하는 컬렉션 타입
+var teamForward: [String: [String]] = ["Tottenham": ["Harry Kane"]];
+teamForward["Man City"] = ["Erling Haaland"];
+teamForward["Arsenal"] = ["Gabriel Jesus"];
+
+teamForward["Tottenham"]?.append("Son");
+
+teamForward.removeValue(forKey: "Arsenal");
+
+teamForward;
+
+// Set : 특정 데이터 타입의 값들을 순서없이 저장하는 리스트 (Any 불가, 중복값 불가)
+var set: Set = Set<Int>();
+set.insert(100);
+set.insert(20);
+set.insert(20);
+
+set; // {100, 20}
