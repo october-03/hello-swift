153ec7dbe02d5bd234414281711264fac5d05482
october-03 <season_autumn@kakao.com>
2023-07-24 12:21:50 +0900

    클래스 상속 정리

--- chapter01/chapter01.playground/Pages/class.xcplaygroundpage/Contents.swift
+++ chapter01/chapter01.playground/Pages/class.xcplaygroundpage/Contents.swift
@@ -24,3 +24,50 @@ var deinitTest: User? = User("deinit Test", 20)
 deinitTest = nil;
 
 october03.information()
+
+// 상속
+/*
+class 이름: 부모 클래스 {
+// 하위 클래스 정의
+}
+*/
+class Account: User {
+  var unit: String = "₩"
+  var amount: Int = 0 {
+    didSet {
+      print("잔액 변동")
+      print("\(oldValue) \(unit) → \(amount) \(unit)")
+    }
+  }
+  
+  init(_ name: String, _ age: Int, amount: Int) {
+    self.amount = amount
+    super.init(name, age)
+  }
+  
+  func deposit (_ value: Int) {
+    self.amount += value;
+  }
+  
+  func withdraw (_ value: Int) {
+    if amount - value < 0 {
+      print("잔고가 부족합니다.")
+      return
+    }
+    self.amount -= value
+  }
+  
+  override func information() {
+    super.information()
+    print("잔액: \(amount) \(unit)")
+  }
+}
+
+var october03Account = Account("김재영", 19, amount: 0)
+october03Account.information()
+
+october03Account.deposit(1200)
+october03Account.withdraw(10000)
+october03Account.withdraw(1000)
+
+october03Account.information()
