0aac7327f909db146391c881dff2da5b2bd3e4e0
october-03 <season_autumn@kakao.com>
2023-07-24 16:32:52 +0900

    타입 캐스팅 정리

--- chapter01/chapter01.playground/Pages/class.xcplaygroundpage/Contents.swift
+++ chapter01/chapter01.playground/Pages/class.xcplaygroundpage/Contents.swift
@@ -71,3 +71,75 @@ october03Account.withdraw(10000)
 october03Account.withdraw(1000)
 
 october03Account.information()
+
+// 타입 캐스팅 : 인스턴스의 타입을 확인하거나 어떠한 클래스의 인스턴스를 해당 클래스 계층 구조의 슈퍼 클래스나 서브 클래스로 취급하는 방법
+class Media {
+  var name: String
+  init(_ name: String) {
+    self.name = name
+  }
+}
+
+class Movie: Media {
+  var director: String
+  init (name: String, director: String) {
+    self.director = director
+    super.init(name)
+  }
+}
+
+class Song: Media {
+  var artist: String
+  init (name: String, artist: String) {
+    self.artist = artist
+    super.init(name)
+  }
+}
+
+class Book: Media {
+  var author: String
+  init (name: String, author: String) {
+    self.author = author
+    super.init(name)
+  }
+}
+
+var movieCount = 0
+var songCount = 0
+var bookCount = 0
+
+let library = [
+  Movie(name: "기생충", director: "봉준호"),
+  Movie(name: "너의 이름은", director: "신카이 마코토"),
+  Song(name: "백야", artist: "M.C the Max"),
+  Song(name: "에필로그", artist: "정승환"),
+  Book(name: "공허한 십자가", author: "히가시노 게이고")
+]
+
+func libraryCount() -> Void {
+  print("현재 도서관은 \(bookCount)권의 책과 \(movieCount)편의 영화, 노래 \(songCount)곡을 보유중입니다.")
+}
+
+for item in library {
+  if item is Movie {
+    movieCount += 1
+  } else if item is Song {
+    songCount += 1
+  } else if item is Book {
+    bookCount += 1
+  }
+}
+
+for item in library {
+  if let movie = item as? Movie {
+    print("제목: \(movie.name) 감독: \(movie.director)")
+  }
+  if let song = item as? Song {
+    print("제목: \(song.name) 가수: \(song.artist)")
+  }
+  if let book = item as? Book {
+    print("제목: \(book.name) 저자: \(book.author)")
+  }
+}
+
+libraryCount()
