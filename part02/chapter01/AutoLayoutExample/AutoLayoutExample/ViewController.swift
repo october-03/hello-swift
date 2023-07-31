fd13186ad23ca03b8b29e7f9fc49acf6654457be
october-03 <season_autumn@kakao.com>
2023-07-31 16:10:37 +0900

    IBOutlet, IBAction 활용해 버튼 클릭시 UIView의 색상이 변경되는 기능 적용

--- part02/chapter01/AutoLayoutExample/AutoLayoutExample/ViewController.swift
+++ part02/chapter01/AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var colorContainer: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func onColorChange(_ sender: UIButton) {
+    self.colorContainer.backgroundColor = UIColor.blue
+    print("onClick Color Change Button")
+  }
+  
 }
 
