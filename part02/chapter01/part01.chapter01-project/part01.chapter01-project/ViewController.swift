42f09c2e839b6dd3b6891902d82cfa2215be35d0
october-03 <season_autumn@kakao.com>
2023-07-31 17:13:03 +0900

    part2-ch01 프로젝트 설정

---
+++ part02/chapter01/part01.chapter01-project/part01.chapter01-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  part01.chapter01-project
+//
+//  Created by 김재영 on 2023/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ part02/chapter01/part01.chapter01-project/part01.chapter01-projectUITests/part01_chapter01_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  part01_chapter01_projectUITestsLaunchTests.swift
+//  part01.chapter01-projectUITests
+//
+//  Created by 김재영 on 2023/07/31.
+//
+
+import XCTest
+
+final class part01_chapter01_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
