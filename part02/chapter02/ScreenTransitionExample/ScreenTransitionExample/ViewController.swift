ad63f18f55f42a0504671b860fb6e280ad60a9d8
october-03 <season_autumn@kakao.com>
2023-08-04 14:51:36 +0900

    viewContoller Life Cycle 예제

--- part02/chapter02/ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
+++ part02/chapter02/ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    print("뷰 로드")
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("뷰가 나타날 예정")
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("뷰가 나타남")
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("뷰가 사라질 예정")
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("뷰가 사라짐")
   }
   
   @IBAction func handleCodePushButton(_ sender: UIButton) {
